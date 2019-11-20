35c6a6c4bef195c8f3e3f9cae1482e474b03bd24
Daniel Young <daniel.young@bluefrontier.co.uk>
2019-11-20 15:24:31 +0000

    chore: updating dependencies

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
-const dotenv = require("dotenv");
+const oop = require("oop-node-common");
 
-dotenv.config();
-
-module.exports = {
-    amqpAddress: process.env.OOP_AMQP_ADDRESS,
-    rendererInputQ: process.env.OOP_RENDERER_INPUT_Q,
-    endpointsExchangeName: process.env.OOP_ENDPOINTS_EXCHANGE_NAME,
-    oopEndpointsQ: process.env.OOP_ENDPOINT_Q,
-};
+module.exports = new oop.Config({
+    amqpAddress: "OOP_AMQP_ADDRESS",
+    rendererInputQ: "OOP_RENDERER_INPUT_Q",
+    endpointsExchangeName: "OOP_ENDPOINTS_EXCHANGE_NAME",
+    oopEndpointsQ: "OOP_ENDPOINT_Q",
+});
