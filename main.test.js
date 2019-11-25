93e982d1440400ec48788ec5526ea354665251e8
Daniel Young <daniel.young@bluefrontier.co.uk>
2019-11-25 14:53:52 +0000

    test: adding more unit test coverage

--- main.test.js
+++ main.test.js
@@ -32,3 +32,33 @@ test("discard transmission", t => {
         t.deepEqual([], published);
     });
 });
+
+test("transmission works", t => {
+    t.plan(1);
+
+    const published = [];
+
+    return new Promise(resolve => {
+        const broker = {
+            consume: (queue, fn) => {
+                return fn({
+                    content: {
+                        uuid: "1234-123456-1234-1234",
+                        tempr: {
+                            template: {
+                                val: "test"
+                            }
+                        }
+                    }
+                }).then(resolve);
+            },
+            publish: (exchange, queue, message) => {
+                published.push(message);
+            }
+        };
+
+        main(broker, {}, console);
+    }).then(() => {
+        t.is(published[0].rendered.val, "test");
+    });
+});
