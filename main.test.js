233e11ae9bf9ec992d2aee58c661836a077d845d
Daniel Young <daniel.young@bluefrontier.co.uk>
2019-11-27 15:46:31 +0000

    feat: adding console to renderer

--- main.test.js
+++ main.test.js
@@ -69,3 +69,37 @@ test("transmission works", t => {
         t.is(published[0].tempr.rendered.val, "test");
     });
 });
+
+test("console log works", t => {
+    t.plan(2);
+
+    const published = [];
+
+    return new Promise(resolve => {
+        const broker = {
+            consume: (queue, fn) => {
+                return fn({
+                    content: {
+                        uuid: "1234-123456-1234-1234",
+                        tempr: {
+                            template: {
+                                val: {
+                                    script: "console.log('foo');",
+                                    language: "js",
+                                }
+                            }
+                        }
+                    }
+                }).then(resolve);
+            },
+            publish: (exchange, queue, message) => {
+                published.push(message);
+            }
+        };
+
+        main(broker, {}, mockLogger);
+    }).then(() => {
+        t.is(published.length, 1);
+        t.is(published[0].tempr.console, "foo\n");
+    });
+});
